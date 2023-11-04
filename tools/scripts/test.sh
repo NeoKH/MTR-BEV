35a40ca7e4c2a97b8bad3f136aa10f753bee6f2d
neokh <konghao078@gmail.com>
2023-11-04 18:08:13 +0800

    update readme

--- tools/scripts/test.sh
+++ tools/scripts/test.sh
@@ -2,12 +2,12 @@
 TAG='5_percent_bev_base'
 EXP='my_1st_exp'
 python ./tools/demo.py \
-    --eval_all --batch_size 2 --workers 4 \
+    --eval_all --batch_size 2 --workers 1 \
     --extra_tag $EXP \
     --cfg_file "./tools/cfgs/$TAG.yaml"
     # --ckpt_id 30
 TAG='5_percent_no_bev'
 python ./tools/demo.py \
-    --eval_all --batch_size 2 --workers 4 \
+    --eval_all --batch_size 4 --workers 4 \
     --extra_tag $EXP \
     --cfg_file "./tools/cfgs/$TAG.yaml"
